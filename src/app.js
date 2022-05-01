120692d31ff6bc5e8073187f2b31efba7c831602
elcode7 <101850419+elcode7@users.noreply.github.com>
2022-04-30 20:11:42 -0700

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
- 
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "08e18f135cb9caaa0a50489b3a535de7";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
