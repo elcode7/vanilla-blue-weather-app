e58485a7b0754422ba3847d6e20964e3ee30f548
elcode7 <101850419+elcode7@users.noreply.github.com>
2022-05-09 12:29:53 -0700

    Url forecast fixed

--- src/app.js
+++ src/app.js
@@ -66,10 +66,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "08e18f135cb9caaa0a50489b3a535de7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}
-  &appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
